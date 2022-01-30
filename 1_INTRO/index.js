6a1638f96648c5a6aeddb7c8ac234643f1d7aa3b
artagnancoder <stayhomer@outlook.com>
2022-01-30 19:44:20 -0300

    Fist class ts code

---
+++ 1_INTRO/index.js
@@ -0,0 +1,8 @@
+var firstName = 'Igor';
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Olá" + name);
+}
+greeting(anotherName);
+//TSC + nomeDoArquivo para compilar TS em JS.
---
+++ 1_INTRO/index.ts
@@ -0,0 +1,13 @@
+const firstName: string = 'Igor'
+const anotherName: number = 1
+const x = true
+
+
+function greeting(name: string) {
+    console.log("Olá" + name);
+
+}
+
+greeting(firstName)
+
+//TSC + nomeDoArquivo para compilar TS em JS.
