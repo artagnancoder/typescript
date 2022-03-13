b24b89c003bc7663cba309772587f488af986434
artagnancoder <81184973+artagnancoder@users.noreply.github.com>
2022-03-13 16:52:02 -0300

    array types

---
+++ 3_AVANÇANDO_EM_TIPOS/index.ts
@@ -0,0 +1,11 @@
+//Arrays
+
+//array de números: number[] 
+
+let numbers: number[] = [1, 2, 3, 4]
+let nums: Array<number> = [1, 2, 3, 4]
+
+//array de strings: string[]
+
+let alphabet: string[] = ['a', 'b', 'c']
+let alpha: Array<string> = ['a', 'b', 'c']
