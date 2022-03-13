8d23e267dcc6312f719432937e63bf7a9eb3ac5b
artagnancoder <81184973+artagnancoder@users.noreply.github.com>
2022-03-13 17:04:15 -0300

    any type

--- 3_AVANÇANDO_EM_TIPOS/index.ts
+++ 3_AVANÇANDO_EM_TIPOS/index.ts
@@ -9,3 +9,11 @@ let nums: Array<number> = [1, 2, 3, 4]
 
 let alphabet: string[] = ['a', 'b', 'c']
 let alpha: Array<string> = ['a', 'b', 'c']
+
+//Any
+
+//Casos de uso: quando o tipo de dado não importa e em arrays com dados múltiplos.
+
+let data: any[] = ['a', 1, 'b', 2]
+
+
